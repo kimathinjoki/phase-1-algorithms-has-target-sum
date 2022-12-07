3b2f4aee02664aa6c96de7bca3fe45c294ffaa7a
kimathinjoki <dfkimathinjoki@gmail.com>
2022-12-07 14:53:55 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,17 +1,44 @@
-function hasTargetSum(array, target) {
-  // Write your algorithm here
-}
+/* First method*/
 
-/* 
+// function hasTargetSum(array,target){
+//   for(let i=0;i<array.length;i++){
+//     const number = target -array[i]
+//     for(let b =i+1;b<array.length; b++){
+//       if(number === array[b])return true
+//     }
+//   }
+//   return false
+
+// }
+
+/*
   Write the Big O time complexity of your function here
-*/
-
-/* 
-  Add your pseudocode here
+  the big O time complexity is O(n*n)
 */
 
 /*
+  Add your pseudocode here
+  take each number in the arrayby iterating
+  add each number to an iterration of the array
+  do a comaprison with the ttarget value
+*/
+
+/*Alternative code*/
+function hasTargetSum(array,target){
+  for(let i= 0; i<array.length;i++){
+      let num = array[i]
+
+      for(let b= i+1; b<array.length;b++){
+          if((num+array[b])=== target) return true
+
+      }
+  }
+  return false
+
+}
+/*
   Add written explanation of your solution here
+  the code iterates through the array taking each number and adding the other numbers of the array comparing it to the target
 */
 
 // You can run `node index.js` to view these console logs
